d01ea2faf561d509c165f2ba89b9c96cf8d1e610
arellanog <arellanog@cent.gov.ar>
2020-08-18 10:51:40 -0300

    Agrego clases de dominio y persistencia usando JPA y MariaDB

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,41 @@
 package com.packt.cardatabase;
 
-import org.springframework.boot.SpringApplication;
+import com.packt.cardatabase.domain.*;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+
+	private final CarRepository carRepository;
+	private final OwnerRepository ownerRepository;
+
+	public CardatabaseApplication(CarRepository carRepository, OwnerRepository ownerRepository) {
+		this.carRepository = carRepository;
+		this.ownerRepository = ownerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			// Add owner objects and save these to db
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
+
+			// Save some cars
+			carRepository.save(new Car("Ford", "Mustang", "Red",
+					"ADF-1121", 2017, 59000, owner1));
+			carRepository.save(new Car("Nissan", "Leaf", "White",
+					"SSJ-3002", 2014, 29000, owner1));
+			carRepository.save(new Car("Toyota", "Prius", "Silver",
+					"KKO-0212", 2018, 39000, owner2));
+		};
+	}
 }
---
+++ src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
@@ -0,0 +1,6 @@
+package com.packt.cardatabase.domain;
+
+import org.springframework.data.repository.*;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
