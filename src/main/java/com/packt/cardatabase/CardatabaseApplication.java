9854d7f664d697b3dab2fcc854017002a1d2f218
arellanog <arellanog@cent.gov.ar>
2020-08-18 12:18:11 -0300

    Creo usuarios de prueba

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -10,10 +10,12 @@ public class CardatabaseApplication {
 
     private final CarRepository carRepository;
     private final OwnerRepository ownerRepository;
+    private final UserRepository userRepository;
 
-    public CardatabaseApplication(CarRepository carRepository, OwnerRepository ownerRepository) {
+    public CardatabaseApplication(CarRepository carRepository, OwnerRepository ownerRepository, UserRepository userRepository) {
         this.carRepository = carRepository;
         this.ownerRepository = ownerRepository;
+        this.userRepository = userRepository;
     }
 
     public static void main(String[] args) {
@@ -36,6 +38,15 @@ public class CardatabaseApplication {
                     "SSJ-3002", 2014, 29000, owner1));
             carRepository.save(new Car("Toyota", "Prius", "Silver",
                     "KKO-0212", 2018, 39000, owner2));
+
+            // username: user password: user
+            userRepository.save(new User("user",
+                    "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
+                    "USER"));
+            // username: admin password: admin
+            userRepository.save(new User("admin",
+                    "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG",
+                    "ADMIN"));
         };
     }
 }
