2900dda93c8744d6dfa4edd9b54131be081c617f
arellanog <arellanog@cent.gov.ar>
2020-08-18 11:42:15 -0300

    Agrego métodos de búsqueda REST a CarRepository

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,7 +1,17 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.*;
+import org.springframework.data.repository.query.*;
+import org.springframework.data.rest.core.annotation.*;
 
+import java.util.*;
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
+    // Fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(@Param("color") String color);
 }
 
